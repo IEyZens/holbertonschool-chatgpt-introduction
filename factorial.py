62dbd3276400727f151f1f94aaa925f02445c6ae
IEyZens <roncint61@gmail.com>
2025-05-05 01:04:06 +0200

    Add file for task 0

---
+++ factorial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Décrémenter n à chaque itération
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <number>")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+        if number < 0:
+            raise ValueError("Number must be non-negative.")
+        f = factorial(number)
+        print(f)
+    except ValueError as e:
+        print("Error:", e)
+        sys.exit(1)
